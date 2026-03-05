4fc10f818f1e8012dcc90fc41271fbb4bc1b4744
chandru682 <chandramohan3472@gmail.com>
2026-03-05 12:35:16 +0530

    Database

--- database.py
+++ database.py
@@ -1,7 +1,16 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
 
-DATABASE_URL = "mysql+pymysql://root:root@localhost/hr_db"
+# load variables from .env file if present
+load_dotenv()
+
+# expect a DATABASE_URL in environment, fallback to local default
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "mysql+pymysql://root:root@localhost/hr_db"
+)
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
